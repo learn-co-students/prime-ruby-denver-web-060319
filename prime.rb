9084f7df4b1c7b001bf2ede79a51d9d1dc33f524
Rj <rikrdo.ortega@gmail.com>
2019-05-13 17:15:31 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+    if number < 2
+        false
+    else
+        (2..number-1).to_a.all? do |each_number|
+            number % each_number != 0
+        end
+    end
+end
