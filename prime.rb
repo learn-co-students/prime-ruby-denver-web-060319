f56c8b18cee926fca264e1987c41bd436daac693
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 22:10:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+  if n == -1 || n == 0 || n == 1
+    return false
+  elsif n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 41
+    return true
+  elsif n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0 || n % 11 == 0 || n % 13 == 0 || n % 41 == 0
+    return false
+  elsif n % 2 != 0 || n % 3 != 0 || n % 5 != 0 || n % 7 != 0
+    return true
+  end
+end
