99fe9bae2323e64a5574bd3ec7f06c48c15852dc
github username <github email address>
2019-06-02 10:53:39 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+def prime?(num)
+   if num == 1
+    return false
+   elsif num < 1 
+    return false
+   else
+    for n in 2..(num-1)
+        if (num%n)==0
+            return false
+        end 
+        
+    end 
+end 
+true
+end 
+
+
+
+
+
+#if num%n==0
+       
+    #return true
+#else
+   #return false
+#end 
