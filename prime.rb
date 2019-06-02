53097173d64efaf45c0b017e9179912c8e575e6c
JangmiTokki <13933554+JangmiTokki@users.noreply.github.com>
2019-06-02 13:29:12 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+    divisor = (1..num).to_a
+    is_divisible = 0
+   if num < 2 
+        return false
+   elsif num == 2
+        return true
+   else
+       for item in divisor
+            if num % item == 0
+                is_divisible += 1
+            end
+        end
+   end
+   if is_divisible == 2
+        return true
+    else 
+        return false
+    end
+end
